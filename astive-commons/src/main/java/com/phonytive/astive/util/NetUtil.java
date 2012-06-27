0e4ff4b5ef5eeec8b53c6dc7eb2d36afa060fecd
Pedro Sanders <sanderspedro@gmail.com>
2012-06-27 07:31:47 -0500

    Added net utility.

---
+++ astive-commons/src/main/java/com/phonytive/astive/util/NetUtil.java
@@ -0,0 +1,85 @@
+/* 
+ * Copyright (C) 2010-2012 PhonyTive LLC
+ * http://astive.phonytive.com
+ *
+ * This file is part of Astive Toolkit
+ *
+ * Astive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Astive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Astive.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Astive, is the core library of Astive Toolkit, the framework for
+// developers wishing to create concise and easy to maintain applications
+// for Asterisk® PBX, even for complex navigation.
+//
+// Copyright (C) 2010-2011 PhonyTive, S.L.
+// http://www.phonytive.com/astive
+//
+// This file is part of Astive
+//
+// Astive is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Astive is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Astive.  If not, see <http://www.gnu.org/licenses/>.
+package com.phonytive.astive.util;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+
+/**
+ * Checks to see if a specific port is available.
+ *
+ * @param port the port to check for availability
+ */
+public class NetUtil {
+
+    public static boolean available(int port) {
+
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+
+            return true;
+        } catch (IOException e) {
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    /*
+                     * should not be thrown
+                     */
+                }
+            }
+        }
+
+        return false;
+    }
+}
