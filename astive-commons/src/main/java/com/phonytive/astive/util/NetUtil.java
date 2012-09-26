d0149933b6365090f2ff825097bcf883988eae00
Pedro Sanders <sanderspedro@gmail.com>
2012-09-26 01:46:49 -0400

    Code refactoring.

--- astive-commons/src/main/java/com/phonytive/astive/util/NetUtil.java
+++ astive-commons/src/main/java/com/phonytive/astive/util/NetUtil.java
@@ -17,27 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Astive.  If not, see <http://www.gnu.org/licenses/>.
  */
-// Astive, is the core library of Astive Toolkit, the framework for
-// developers wishing to create concise and easy to maintain applications
-// for Asterisk® PBX, even for complex navigation.
-//
-// Copyright (C) 2010-2011 PhonyTive, S.L.
-// http://www.phonytive.com/astive
-//
-// This file is part of Astive
-//
-// Astive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Astive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Astive.  If not, see <http://www.gnu.org/licenses/>.
 package com.phonytive.astive.util;
 
 import java.io.IOException;
@@ -45,13 +24,13 @@ import java.net.DatagramSocket;
 import java.net.ServerSocket;
 
 /**
- * Checks to see if a specific port is available.
+ * Checks to see if a specific port is isPortAvailable.
  *
  * @param port the port to check for availability
  */
 public class NetUtil {
 
-    public static boolean available(int port) {
+    public static boolean isPortAvailable(int port) {
 
         ServerSocket ss = null;
         DatagramSocket ds = null;
