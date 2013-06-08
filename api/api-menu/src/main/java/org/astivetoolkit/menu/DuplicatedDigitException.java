0b244e6e9034556798fdf27b44c9a296b0770903
psanders <psanders@psanders>
2013-06-08 08:51:34 -0500

    Fix: Class DuplicatedDigitsException  changed to DuplicatedDigitException.

---
+++ api/api-menu/src/main/java/org/astivetoolkit/menu/DuplicatedDigitException.java
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (C) 2010-2013 by PhonyTive LLC (http://phonytive.com)
+ * http://astivetoolkit.org
+ *
+ * This file is part of Astive Toolkit(ATK)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.astivetoolkit.menu;
+
+import org.astivetoolkit.util.AppLocale;
+
+/**
+ * This exception is trigger when user attempts to insert more than one menu or
+ * menu item into the same menu level using the same </code>Digit</code>.
+ * 
+ * @since 1.0.0
+ */
+public class DuplicatedDigitException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private static final String MSG = AppLocale
+                    .getI18n("exeption.DuplicatedKeyInMenuException");
+
+    /**
+     * Creates a new instance of DuplicatedDigitException.
+     */
+    public DuplicatedDigitException() {
+        super(DuplicatedDigitException.MSG);
+    }
+}
